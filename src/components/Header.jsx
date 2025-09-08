24f1e92744a13f721392be188842a1f670821011
seaminor505 <seaminor505@gmail.com>
2025-09-08 22:00:19 +0900

    Style: Apply mobile-first design to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Search } from 'lucide-react';
+import profile from '../assets/profile.jpg';
 
 function Header() {
   return (
-    <header>
+    <header className="px-4 py-2 h-15 flex justify-between items-center">
       <a href="#">
-        <h1>Story Canvas</h1>
+        <h1 className="text-lg font-bold">Story Canvas</h1>
       </a>
 
-      <div>
+      <div className="flex items-center">
         {/* 모바일 서치 */}
-        <button>
-          <Search />
+        <button className="p-1 mr-2 rounded-full cursor-pointer">
+          <Search size={24} />
         </button>
 
         {/* 탭+  */}
@@ -21,11 +22,20 @@ function Header() {
         </div> */}
 
         {/* 게스트 */}
-        <a href="">로그인</a>
+        {/* <a href="#" className="block px-1 py-1 rounded font-bold">
+          로그인
+        </a> */}
 
         {/* 유저 프로필 */}
-        <a href="">
-          <img src="" alt="" />
+        <a
+          href="#"
+          className="w-8 h-8 rounded-full overflow-hidden flex justify-center items-center border border-gray-300 bg-gray-100 overflow-hidden"
+        >
+          <img
+            src={profile}
+            alt="user's profile"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
     </header>
